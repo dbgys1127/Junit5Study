3dbea52ccfe8631c598c1bf82f337ac53d9d2808
dbgys1127 <dbgys1127@gmail.com>
2023-03-06 10:57:07 +0900

    feat : Book entity 생성

--- src/main/java/com/example/demo/domain/Book.java
+++ src/main/java/com/example/demo/domain/Book.java
@@ -1,4 +1,27 @@
 package com.example.demo.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(length = 50, nullable = false)
+    private String title;
+    @Column(length = 20,nullable = false)
+    private String author;
+
+    @Builder
+    public Book(long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
 }
